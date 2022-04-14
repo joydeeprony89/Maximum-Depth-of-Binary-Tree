453e2efc5121a9f44aa2465ae002ed309b2ba59a
jd_786 <jaydeep_shil@certree.com>
2022-04-14 18:35:16 +0530

    Add project files.

---
+++ Maximum Depth of Binary Tree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Maximum_Depth_of_Binary_Tree
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+
+    public class TreeNode
+    {
+      public int value;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val)
+      {
+        value = val;
+        left = right = null;
+      }
+    }
+
+    public int MaxDepth(TreeNode root)
+    {
+      return MaxDepthHelper(root, 0);
+    }
+
+    private int MaxDepthHelper(TreeNode root, int max)
+    {
+      if (root == null) return max;
+      max++;
+      int maxLeft = MaxDepthHelper(root.left, max);
+      int maxRight = MaxDepthHelper(root.right, max);
+      return Math.Max(maxLeft, maxRight);
+    }
+  }
+}
